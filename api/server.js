ec0db3e1a0c1da7ddaa00b93b89e7d7633cb0aab
Kris Thornton <Kristhornton@ymail.com>
2023-09-15 22:07:43 -0700

    adding more server req

--- api/server.js
+++ api/server.js
@@ -3,6 +3,34 @@
 const express = require('express')
 const User = require('./users/model')
 const server = express()
+server.use(express.json())
+
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        
+        res.status(422).json({
+            
+            message: 'name and bio required'
+        })
+    }else{
+        User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+    
+})
+
+
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -36,6 +64,11 @@ server.get('/api/users/:id', (req, res) => {
         })
     })
 })
+
+
+
+
+
 
 server.use('*', (req, res) => {
     res.status(404).json({

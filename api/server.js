9e017be759b5d1906e564dfd0dd1d4c8fdf8baf0
Kris Thornton <Kristhornton@ymail.com>
2023-09-17 08:35:52 -0700

    finished all endpoints.

--- api/server.js
+++ api/server.js
@@ -66,23 +66,54 @@ server.get('/api/users/:id', (req, res) => {
 })
 
 
-server.delete('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            if (!user) {
+server.delete('/api/users/:id', async (req, res) => {
+    const possibleUser = await User.findById(req.params.id)
+        
+            if (!possibleUser) {
                 res.status(404).json({
                     message: 'The user with the specified ID does not exist',
                 })
+                res.json(possibleUser)
+            }else{
+                const deletedUser = await User.remove(possibleUser.id)
+                res.status(200).json(deletedUser)
             }
-            res.json(user)
+            
+        
+         })
+
+
+
+server.put('/api/users/:id', async (req, res) => {
+    try{
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        }else {
+            if(!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            }else {
+               const updatedUser = await User.update(
+                req.params.id, 
+                req.body,
+                )
+               res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating users',
+            err: err.message,
+            stack: err.stack,
         })
-        .catch(err => {
-            res.status(500).json({
-                message: 'error getting user',
-                err: err.message,
-        })
-    })
+    }
 })
+
+
 
 
 
